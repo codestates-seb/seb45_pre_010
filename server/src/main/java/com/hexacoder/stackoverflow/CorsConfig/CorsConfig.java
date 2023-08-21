2df9e5d810483d8f80a31acef86f01e4ad81d552
DNDN <130131777+2DNDN@users.noreply.github.com>
2023-08-21 15:35:17 +0900

    Cors 전부허용

--- server/src/main/java/com/hexacoder/stackoverflow/CorsConfig/CorsConfig.java
+++ server/src/main/java/com/hexacoder/stackoverflow/CorsConfig/CorsConfig.java
@@ -1,32 +1,20 @@
 package com.hexacoder.stackoverflow.CorsConfig;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-    @Configuration
-    public class CorsConfig {
+@Configuration
 
-        @Bean
-        public CorsFilter corsFilter() {
-            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-            CorsConfiguration config = new CorsConfiguration();
-            config.addAllowedOrigin("/**");
-            config.addAllowedHeader(CorsConfiguration.ALL);
-            config.addAllowedMethod(HttpMethod.GET);
-            config.addAllowedMethod(HttpMethod.POST);
-            config.addAllowedMethod(HttpMethod.HEAD);
-            config.addAllowedMethod(HttpMethod.PUT);
-            config.addAllowedMethod(HttpMethod.DELETE);
-            config.addAllowedMethod(HttpMethod.TRACE);
-            config.addAllowedMethod(HttpMethod.OPTIONS);
-            config.setAllowCredentials(true);
-            config.setMaxAge(3600L);
+public class CorsConfig implements WebMvcConfigurer {
 
-            source.registerCorsConfiguration("/**", config);
-            return new CorsFilter(source);
-        }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
+}
