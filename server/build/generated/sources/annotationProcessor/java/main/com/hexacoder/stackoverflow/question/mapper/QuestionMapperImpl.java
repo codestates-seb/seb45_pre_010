f701b74f1c5cdfaa5435c73ed66a11ee0105b9bc
jaylee0326 <jay_iuventus@naver.com>
2023-08-22 03:33:07 +0900

    Fix: mapper target 수정

--- server/build/generated/sources/annotationProcessor/java/main/com/hexacoder/stackoverflow/question/mapper/QuestionMapperImpl.java
+++ server/build/generated/sources/annotationProcessor/java/main/com/hexacoder/stackoverflow/question/mapper/QuestionMapperImpl.java
@@ -17,7 +17,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-08-21T14:54:32+0900",
+    date = "2023-08-22T03:13:36+0900",
     comments = "version: 1.5.3.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.2.1.jar, environment: Java 17.0.8 (IBM Corporation)"
 )
 @Component
