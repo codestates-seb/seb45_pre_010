6dcc3d5da52c5f9a189477e4835813e0c326a1de
jaylee <jay_iuventus@naver.com>
2023-08-22 03:54:04 +0900

    Update UserMapper.java

--- server/src/main/java/com/hexacoder/stackoverflow/user/mapper/UserMapper.java
+++ server/src/main/java/com/hexacoder/stackoverflow/user/mapper/UserMapper.java
@@ -4,11 +4,17 @@ import org.mapstruct.Mapper;
 
 import com.hexacoder.stackoverflow.user.dto.UserDto;
 import com.hexacoder.stackoverflow.user.entity.UserEntity;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    @Mapping(target = "userId", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
     UserEntity postDtoToEntity(UserDto.Post userPostDto);
-    UserDto.Response userToUserResponseDto(UserEntity user);
-    UserEntity userPatchDtoToUser(UserDto.Patch userPatchDto);
 
+    UserDto.Response userToUserResponseDto(UserEntity user);
+
+    @Mapping(target = "email", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    UserEntity userPatchDtoToUser(UserDto.Patch userPatchDto);
 }
